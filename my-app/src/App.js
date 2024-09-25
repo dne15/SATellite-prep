6df5d675977944e2e77e76780d32195f37c24887
Luisde Cespedes <lewisofbristol@gmail.com>
2024-09-25 14:32:04 +0200

    first page done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,62 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import HomePage from './components/HomePage';
-import LaunchPage from './components/LaunchPage';
-import MissionControlPage from './components/MissionControlPage';
-import GalaxyExplorerPage from './components/GalaxyExplorerPage';
-import PlanetExplorerPage from './components/PlanetExplorerPage';
-import TheRomansPage from './components/TheRomansPage';
-import ChatBotPage from './components/ChatBotPage';
 
+import MissionControlPage from './components/MissionControlPage';
 
 function App() {
   return (
     <Router>
       <div className="container">
-        {/* <div className="logo-row">
-          <img className="logoimage" src="/header.png" alt="header" />
-        </div> */}
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/launch">Launch</Link></li>
-            <li><Link to="/mission-control">Mission Control</Link></li>
-            <li><Link to="/galaxy-explorer">Galaxy Explorer</Link></li>
-            <li><Link to="/planet-explorer">Planet Explorer</Link></li>
-            <li><Link to="/the-romans">The Romans</Link></li>
-            <li><Link to="/chatbot">Chat Bot</Link></li>
-          </ul>
-        </nav>
+        {/* Fondo y texto */}
+        <div className="Skyandtext">
+          <Link to="/missioncontrol">
+            <img className="background" src="/background.png" alt="Sky background" />
+          </Link>
+        </div>
 
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/launch" element={<LaunchPage />} />
-          <Route path="/mission-control" element={<MissionControlPage />} />
-          <Route path="/galaxy-explorer" element={<GalaxyExplorerPage />} />
-          <Route path="/planet-explorer" element={<PlanetExplorerPage />} />
-          <Route path="/the-romans" element={<TheRomansPage />} />
-          <Route path="/chatbot" element={<ChatBotPage />} />
-        </Routes>
+        {/* Cohete y bolas de humo */}
+        <div className="rocketandballs">
+          <img className="rocket" src="/rocket.png" alt="Rocket" />
+          <div className="balls">
+            {/* Bolas de humo */}
+            <div className="ball">
+              <img src="/ball1.png" alt="Smoke Ball 1" />
+            </div>
+            <div className="ball">
+              <img src="/ball2.png" alt="Smoke Ball 2" />
+            </div>
+            <div className="ball">
+              <img src="/ball3.png" alt="Smoke Ball 3" />
+            </div>
+          </div>
+        </div>
       </div>
+
+      {/* Rutas */}
+      <Routes>
+        <Route path="/missioncontrol" element={<MissionControlPage />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-// function App() {
-
-
-//   return (
-//     <div className="container">
-//       <div className="Sky+text">
-//         <img className="logoimage" src="/header.png" alt="header" />
-//         <h1>Im working</h1>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
