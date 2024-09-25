09d4719ca1d78264ce5f7eb867399a6f856eb6dd
Luisde Cespedes <lewisofbristol@gmail.com>
2024-09-25 16:35:37 +0200

    routing sorted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,42 +1,45 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
-import MissionControlPage from './components/MissionControlPage';
+import MissionControlPage from './pages/MissionControlPage';
 
 function App() {
   return (
     <Router>
-      <div className="container">
-        {/* Fondo y texto */}
-        <div className="Skyandtext">
-          <Link to="/missioncontrol">
-            <img className="background" src="/background.png" alt="Sky background" />
-          </Link>
-        </div>
-
-        {/* Cohete y bolas de humo */}
-        <div className="rocketandballs">
-          <img className="rocket" src="/rocket.png" alt="Rocket" />
-          <div className="balls">
-            {/* Bolas de humo */}
-            <div className="ball">
-              <img src="/ball1.png" alt="Smoke Ball 1" />
-            </div>
-            <div className="ball">
-              <img src="/ball2.png" alt="Smoke Ball 2" />
-            </div>
-            <div className="ball">
-              <img src="/ball3.png" alt="Smoke Ball 3" />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Rutas */}
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/missioncontrol" element={<MissionControlPage />} />
       </Routes>
     </Router>
+  );
+}
+
+function HomePage() {
+  return (
+    <div className="container">
+      <div className="Skyandtext">
+        <Link to="/missioncontrol">
+          <img className="background" src="/background.png" alt="Sky background" />
+        </Link>
+      </div>
+
+      {/* Cohete y bolas de humo */}
+      <div className="rocketandballs">
+        <img className="rocket" src="/rocket.png" alt="Rocket" />
+        <div className="balls">
+          {/* Bolas de humo */}
+          <div className="ball">
+            <img src="/ball1.png" alt="Smoke Ball 1" />
+          </div>
+          <div className="ball">
+            <img src="/ball2.png" alt="Smoke Ball 2" />
+          </div>
+          <div className="ball">
+            <img src="/ball3.png" alt="Smoke Ball 3" />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
